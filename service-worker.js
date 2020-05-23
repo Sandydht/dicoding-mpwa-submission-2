77b2e23b76612ca8b7ea9d4be6424132514d6507
Sandydht <48269439+Sandydht@users.noreply.github.com>
2020-05-23 16:08:18 +0700

    commit

--- service-worker.js
+++ service-worker.js
@@ -42,7 +42,7 @@ self.addEventListener("fetch", function (event) {
         );
     } else {
         event.respondWith(
-            caches.match(event.request)
+            caches.match(event.request, { ignoreSearch: true })
                 .then(function (response) {
                     return response || fetch(event.request);
                 })
